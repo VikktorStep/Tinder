de30fad976657279e68a6db3e06a02386c9cbea6
VikktorStep <vikktor.stepanov@gmail.com>
2024-06-06 19:55:34 +0300

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Mac on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
